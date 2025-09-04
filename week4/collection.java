d517220b5d364ce48d3299902c68f68fda1013df
smruti123-git-tech <smrutiranjanmahanta5@gmail.com>
2025-09-05 00:15:26 +0530

    final_outcome

--- week4/collection.java
+++ week4/collection.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class collection {
@@ -18,16 +20,23 @@ public class collection {
         //     System.out.println(i);
         // }
 
-        Set<Integer> nums=new HashSet<Integer>();
-        nums.add(4);
-        nums.add(4);
-        nums.add(3);
-        nums.add(7);
-        nums.add(10);
+        // Set<Integer> nums=new HashSet<Integer>();
+        // nums.add(4);
+        // nums.add(4);
+        // nums.add(3);
+        // nums.add(7);
+        // nums.add(10);
 
-        for(int i:nums){
-           System.out.println(i);
-        }
-        System.out.println(nums.get(2));
+        // for(int i:nums){
+        //    System.out.println(i);
+        // }
+
+        Map <String,Integer> student=new HashMap<>();
+        student.put("smruti", 23);
+        student.put("raistar", 25);
+        student.put("salik", 76);
+        student.put("martin", 43);
+        System.out.println(student);
+       
     }
 }
